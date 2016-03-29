42a68be5fedbf32f72f791ac9bdaefd301beb5ab
shark-S <sunilsangwan0098@gmail.com>
2016-03-29 20:55:16 +0530

    Added working speech_to_text_converter

---
+++ speech_to_text-converter/README.md
@@ -0,0 +1,3 @@
+Jarvis for covert speech into text use for
+	1.  terminal uses.
+	2.  open a application
---
+++ speech_to_text-converter/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+import pyaudio
+import subprocess
+import os
+
+# audio message from user using speech recognition
+r = sr.Recognizer()
+with sr.Microphone() as source:
+	print ("Hey this is Jarvis what i can do for you")
+	#voice message is store in audio
+	audio = r.listen(source)
+
+
+# here we use google's api which convert audio into text
+
+try:
+	# text is stored in command
+	command = r.recognize_google(audio)
+	#print (command)
+	# convert string in lower case letter 
+	command=command.lower()
+	#print (command)
+	try:
+		test = subprocess.Popen([command], stdout=subprocess.PIPE)
+		output = test.communicate()[0]
+	except:
+		os.system(command)
+	#except print ("can't execute command")
+		
+
+
+
+
+except sr.UnknownValueError:
+	print ("Google speech recognizer can not understand audio")
+except sr.RequestError as e:
+	print ("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+
+
